b6579e382901c5b9ac5827e36311ccb9667397f9
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-11-01 21:00:02 -0700

    styled page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const searchEl = document.querySelector('.form-control');
 const handleSearch = (event) => {
     event.preventDefault();
     let searchInput = searchEl.value.trim();
-    let weatherURL = `api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=aec02913dbb17f078614aebbba4d5805`;
+    let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=aec02913dbb17f078614aebbba4d5805`;
     fetch(weatherURL)
         .then(function(response){
             return response.json()
