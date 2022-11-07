5e8168ccdf8a287415c2a995d4e97e541c47732b
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-11-07 13:52:29 -0800

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,28 @@ const cityInfo = document.querySelector(".city-info");
 const fiveDay = document.querySelector(".five-day");
 const recentSearch = document.querySelector(".recent-search");
 
-let count = 0;
-let template = "";
-var getCurrent = JSON.parse(localStorage.getItem("recentCurrent")) || [];
-var getForecast = JSON.parse(localStorage.getItem("recentForecast")) || [];
-
-const handleSearch = (event) => {
-  event.preventDefault();
-  let searchInput = searchEl.value.trim();
-  storeData(searchInput);
-  let weatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInput}&limit=1&appid=aec02913dbb17f078614aebbba4d5805`;
+//function to fetch the API
+const fetchAPI = (randomCity) => {
+  let weatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${randomCity}&limit=1&appid=aec02913dbb17f078614aebbba4d5805`;
   fetch(weatherURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       displayCurrentDate(data);
-      displayForecast(event);
+      displayForecast(randomCity);
     });
-   
-    let addButton = document.createElement("button");
-    addButton.textContent = searchInput;
-    addButton.type="button";
-    addButton.classList = "btn btn-secondary col-12 mt-3";
-    recentSearch.appendChild(addButton);
 };
 
+//when search button is clicked, this function takes the search input, stores it, and runs it through the fetchAPI function
+const handleSearch = (event) => {
+  event.preventDefault();
+  let searchInput = searchEl.value.trim();
+  storeData(searchInput);
+  fetchAPI(searchInput);
+};
+
+//this function gets the latitude and longitude from the geolocator API. then uses that data to get the current weather for the city
 const displayCurrentDate = (data) => {
   let latitude = data[0].lat;
   let longitude = data[0].lon;
@@ -42,50 +38,78 @@ const displayCurrentDate = (data) => {
       const date = new Date();
       let todaysDate = date.toLocaleDateString();
       cityInfo.innerHTML = `
-     <h2>${data.name} (${todaysDate}) <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"><h2>
-     <h5>Temp: ${data.main.temp}°F<h5>
-     <h5>Wind: ${data.wind.speed} MPH<h5>
+      <h2>${data.name} (${todaysDate}) <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"><h2>
+      <h5>Temp: ${data.main.temp}°F<h5>
+      <h5>Wind: ${data.wind.speed} MPH<h5>
      <h5>Humidity: ${data.main.humidity} %<h5>`;
     });
 };
 
-const displayForecast = (event) => {
-  event.preventDefault();
-  let searchInput = searchEl.value.trim();
-  console.log(searchInput)
-  let fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=aec02913dbb17f078614aebbba4d5805&units=imperial`;
+//function to get the 5 day forecast for the city
+const displayForecast = (randomCity) => {
+  let fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${randomCity}&appid=aec02913dbb17f078614aebbba4d5805&units=imperial`;
   fetch(fiveDayForecast)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
+      fiveDay.innerHTML = "";
+      let template = "";
+      let count = 0;
       data.list.forEach(function (list) {
-        if ((list.dt_txt.split(" ")[1] == "12:00:00" && count < 6)) {
-            count++;
+        if (list.dt_txt.split(" ")[1] == "12:00:00" && count < 6) {
+          count++;
           template += `
-            <div class="card col-2 mt-3">
+          <div class="card col-2 mt-3">
                 <div class="card-body">
-                    <h5>${list.dt_txt.split(" ")[0]}</h5>
-                    <h5><img src="http://openweathermap.org/img/w/${list.weather[0].icon}.png"><h5>
+                <h5>${list.dt_txt.split(" ")[0]}</h5>
+                <h5><img src="http://openweathermap.org/img/w/${
+                  list.weather[0].icon
+                }.png"><h5>
                     <h5>${list.main.temp}°F</h5>
                     <h5>${list.wind.speed} MPH</h5>
                     <h5>${list.main.humidity} %</h5>
                 </div>
-            </div>`;
+                </div>`;
           fiveDay.innerHTML = template;
         }
       });
     });
 };
 
-const storeData = (lastSearchData) => {
-    getCurrent.push(lastSearchData)
-    localStorage.setItem('recentCurrent', JSON.stringify(getCurrent))
-}
+//when recent search button is clicked, this function gets the current weather and forecast
+const handleStorageClick = (event) => {
+  event.preventDefault();
+  fetchAPI(event.target.textContent);
+};
 
+//function to handle local storage
+const storeData = (lastSearchData) => {
+  let getCurrent = JSON.parse(localStorage.getItem("recentCurrent")) || [];
+  if (!getCurrent.includes(lastSearchData)) {
+    getCurrent.push(lastSearchData);
+  }
+  if (getCurrent.length > 5) {
+    getCurrent.shift();
+  }
+  localStorage.setItem("recentCurrent", JSON.stringify(getCurrent));
+  loadData();
+};
+
+//function to load local storage and add recent search button
+const loadData = () => {
+  var getCurrent = JSON.parse(localStorage.getItem("recentCurrent")) || [];
+  recentSearch.innerHTML = "";
+  for (i = 0; i < getCurrent.length; i++) {
+    let addButton = document.createElement("button");
+    addButton.textContent = getCurrent[i];
+    addButton.type = "button";
+    addButton.classList = "btn btn-secondary col-12 mt-3";
+    addButton.addEventListener("click", handleStorageClick);
+    recentSearch.appendChild(addButton);
+  }
+};
+
+//triggers handleSearch function with searchButton is clicked
 searchButton.addEventListener("click", handleSearch);
-// recentSearch.addEventListener('click', function (event){
-//     var renderPastSearch = event.target.textContent
-//     displayCurrentDate(renderPastSearch)
-//     displayForecast(renderPastSearch)
-// })
+loadData();
