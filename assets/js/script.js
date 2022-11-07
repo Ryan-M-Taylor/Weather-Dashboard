a30a79173e9621856e75d44f21f7fbf6f4dc8a69
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-11-06 18:51:37 -0800

    working on local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@ const cityInfo = document.querySelector(".city-info");
 const fiveDay = document.querySelector(".five-day");
 const recentSearch = document.querySelector(".recent-search");
 
+let count = 0;
+let template = "";
+
 const handleSearch = (event) => {
   event.preventDefault();
   let searchInput = searchEl.value.trim();
@@ -49,17 +52,12 @@ const displayForecast = (event) => {
   event.preventDefault();
   let searchInput = searchEl.value.trim();
   let fiveDayForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${searchInput}&appid=aec02913dbb17f078614aebbba4d5805&units=imperial`;
-  console.log(fiveDayForecast)
   fetch(fiveDayForecast)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-      
-      let count = 0;
-      let template = "";
-
      
       data.list.forEach(function (list) {
         if ((list.dt_txt.split(" ")[1] == "12:00:00" && count < 6)) {
@@ -80,9 +78,21 @@ const displayForecast = (event) => {
     });
 };
 
-const storeData = (lastSearchData) => {
-    localStorage.setItem('recent', JSON.stringify(lastSearchData))
-}
+// const storeData = ({arg1:lastSearchData, arg2:lastForecastData}) => {
+//     localStorage.setItem('recentCurrent', JSON.stringify(lastSearchData))
+//     localStorage.setItem('recentForecast', JSON.stringify(lastForecastData))
+// }
 
 searchButton.addEventListener("click", handleSearch);
 // searchButton.addEventListener('click', storeData)
+
+// searchButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     handleSearch(event);
+//     var city = { name: searchInput.value};
+//     var getCurrent = JSON.parse(localStorage.getItem("recentCurrent")) || [];
+//     var getForecast = JSON.parse(localStorage.getItem("recentForecast")) || [];
+//     getCurrent.push(city);
+//     getForecast.push(city)
+//     searchInput.value = "";
+//     });
