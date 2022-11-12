8aaa870d1530932550a8074ffaa3211600d6f15b
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-11-11 17:40:16 -0800

    added s to http

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const recentSearch = document.querySelector(".recent-search");
 
 //function to fetch the API
 const fetchAPI = (randomCity) => {
-  let weatherURL = `http://api.openweathermap.org/geo/1.0/direct?q=${randomCity}&limit=1&appid=aec02913dbb17f078614aebbba4d5805`;
+  let weatherURL = `https://api.openweathermap.org/geo/1.0/direct?q=${randomCity}&limit=1&appid=aec02913dbb17f078614aebbba4d5805`;
   fetch(weatherURL)
     .then(function (response) {
       return response.json();
@@ -38,7 +38,7 @@ const displayCurrentDate = (data) => {
       const date = new Date();
       let todaysDate = date.toLocaleDateString();
       cityInfo.innerHTML = `
-      <h2>${data.name} (${todaysDate}) <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"><h2>
+      <h2>${data.name} (${todaysDate}) <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"><h2>
       <h5>Temp: ${data.main.temp}°F<h5>
       <h5>Wind: ${data.wind.speed} MPH<h5>
      <h5>Humidity: ${data.main.humidity} %<h5>`;
@@ -63,7 +63,7 @@ const displayForecast = (randomCity) => {
           <div class="card col-2 mt-3">
                 <div class="card-body">
                 <h5>${list.dt_txt.split(" ")[0]}</h5>
-                <h5><img src="http://openweathermap.org/img/w/${
+                <h5><img src="https://openweathermap.org/img/w/${
                   list.weather[0].icon
                 }.png"><h5>
                     <h5>${list.main.temp}°F</h5>
